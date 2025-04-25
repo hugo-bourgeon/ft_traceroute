986c48166ac1e9284be55800daaab4dd8a76d4fa
hubourge <hulo1920@gmail.com>
2025-04-25 19:20:55 +0200

    add: implement parsing function for command-line arguments

---
+++ srcs/parsing.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hubourge <hubourge@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/25 17:05:00 by hubourge          #+#    #+#             */
+/*   Updated: 2025/04/25 19:17:19 by hubourge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_traceroute.h"
+
+void	parsing(int argc, char **argv, t_traceroute *traceroute)
+{
+	if (argc < 2)
+	{
+		fprintf(stderr, "Usage: %s <hostname>\n", argv[0]);
+		free_all(EXIT_FAILURE, traceroute);
+	}
+
+	traceroute->hostname = strdup(argv[1]);
+	if (!traceroute->hostname)
+	{
+		fprintf(stderr, "strdup error\n");
+		free_all(EXIT_FAILURE, traceroute);
+	}
+	(void)traceroute;
+}
